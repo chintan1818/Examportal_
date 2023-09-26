97bafff1527af0b3994b40d4bf470f025273714d
Chintan <mendparachintan39@gmail.com>
2023-09-25 19:55:01 -0700

    last changes

--- examserver/examserver/src/main/java/com/exam/controller/QuizController.java
+++ examserver/examserver/src/main/java/com/exam/controller/QuizController.java
@@ -6,7 +6,6 @@ import com.exam.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashSet;
 
 @RestController
@@ -74,6 +73,7 @@ public class QuizController {
         category.setcId(catId);
         return ResponseEntity.ok(quizService.getActiveQuizesOfCategoty(category));
     }
+
 
 
 
